e07647aaff091dcbf517175a79b80147a933faca
rli9 <rli9>
2015-05-26 14:25:36 +0800

    Add follow mode of cherokey car

--- src/cherokey_car/scripts/car.py
+++ src/cherokey_car/scripts/car.py
@@ -71,12 +71,41 @@ class Car(object):
 
         action = eval("{%s}" % action)
 
-        actions = {"w": self.move_forward, "s": self.move_backward, "a": self.turn_left, "d": self.turn_right}
+        actions = {"forward": self.move_forward, "backward": self.move_backward, "left": self.turn_left, "right": self.turn_right, "follow": self.follow}
 
         direction = action.pop('direction')
         actions[direction](**action)
         # delay(3000)
         self.stop()
+
+    def follow(self, **args):
+        if 'width' not in args:
+            args['width'] = 1280
+        if 'height' not in args:
+            args['height'] = 720
+        half_width = args['width'] / 2
+        quater_width = args['width'] / 4
+
+        half_height = args['height'] / 2
+        quater_height = args['height'] / 4
+
+        tolerant_error_width = args['width'] / 8
+        tolerant_error_height = args['height'] / 8
+
+        object_region_x = ((args['object_x'] - half_width) + quater_width - tolerant_error_width) / quater_width
+        object_region_y = ((args['object_y'] - half_height) + quater_height - tolerant_error_height) / quater_height
+
+        rospy.loginfo('Car.follow(object_region_x=%d, object_region_y=%d)' % (object_region_x, object_region_y))
+
+        if object_region_x > 0:
+            self.turn_right(rotation=50000 * object_region_x)
+        elif object_region_x < 0:
+            self.turn_left(rotation=50000 * object_region_x)
+
+        if object_region_y > 0:
+            self.move_forward(distance=50000 * object_region_y)
+        elif object_region_y < 0:
+            self.move_backward(distance=50000 * object_region_y)
 
     def stop(self):
         self.right_motor.stop()
@@ -88,7 +117,7 @@ class Car(object):
         self.lights[light].on()
 
         if 'speed' not in args:
-            args["speed"] = 80
+            args["speed"] = 100
         rospy.loginfo('Car.move(%s, %s, %s)' % (direction, light, args))
 
         speed = int(args["speed"])
@@ -105,18 +134,18 @@ class Car(object):
             self.stop()
             self.lights[light].off()
 
-    move_forward = method_partial(move, HIGH, "head")
-    move_backward = method_partial(move, LOW, "tail")
+    move_forward = method_partial(move, LOW, "head")
+    move_backward = method_partial(move, HIGH, "tail")
 
     def turn(self, left_direction, right_direction, light, **args):
         self.lights[light].on()
 
         if 'speed' not in args:
-            args["speed"] = {"left": 80, "right": 80}
+            args["speed"] = {"left": 100, "right": 100}
         rospy.loginfo('Car.move(%s, %s, %s, %s)' % (left_direction, right_direction, light, args))
 
         left_speed = int(args["speed"]["left"])
-        right_speed = int(args["speed"]["left"])
+        right_speed = int(args["speed"]["right"])
 
         self.left_motor.run(left_direction, left_speed)
         self.right_motor.run(right_direction, right_speed)
