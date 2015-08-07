4230f35dbfb1da28c197b49c561dc468185bd053
Simon Zheng <bucketzxm@gmail.com>
2015-08-07 16:47:15 +0800

    fix bug in watch_file and add socket connect

---
+++ src/car_control_manual/scratch/connect_to_host.py
@@ -0,0 +1,18 @@
+__author__ = 'Simon Zheng'
+
+"""Transfer command data from scratch to linux host
+"""
+
+import socket
+
+
+class Server(object):
+    pass
+
+
+class Client(object):
+    pass
+
+
+if __name__ == "__main__":
+    pass
--- src/car_control_manual/scratch/watch_file.py
+++ src/car_control_manual/scratch/watch_file.py
@@ -6,8 +6,7 @@ from __future__ import print_function
     3. unzip *.zip file and read json data from project.json
 """
 
-
-import sys, time, logging, os, zipfile
+import sys, time, logging, os, zipfile, json
 import watchdog
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
@@ -53,17 +52,29 @@ class WatchFile(object):
     def _unpack(self, filename):
         # first rename suffix to zip file
         # may not work on linux
-        new_name = filename.split('.')[1] + '.zip'
+        filename = filename[2:] if filename.startswith('.\\') else filename
+        filename = filename.lstrip()
+        new_name = filename.split('.')[0] + '.zip'
         new_name = new_name[1:] if new_name.startswith('\\') else new_name
+
+        print('Old name:', filename, ' New name:', new_name)
+        # waiting for operating sys create the file
+        time.sleep(3)
         os.rename(filename, new_name)
 
         zip_file = zipfile.ZipFile(new_name, 'r')
         json_data = ""
         for name in zip_file.namelist():
             if name == "project.json":
-                file = open(name, 'r')
+                file = zip_file.open(name, 'r')
                 json_data = "".join(file.readlines())
-        return json_data
+        return self.get_cmd(json_data)
+
+    def get_cmd(self, json_data):
+        jsonfy_data = json.loads(json_data)
+        child = jsonfy_data['children'][0]
+        scripts = child['scripts']
+        return scripts
 
 
 if __name__ == "__main__":
