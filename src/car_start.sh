61dbb2ebf27fd22bbd00c4386779c73a2bab3e90
Simon Zheng <bucketzxm@gmail.com>
2015-08-27 09:34:20 +0800

    import car start scripts

--- src/car_start.sh
+++ src/car_start.sh
@@ -1,14 +1,30 @@
-echo starting home setup env
+echo "source ros_catkin_ws setup.bash..."
 source /home/flex/ros_catkin_ws/devel/setup.bash
-echo [ok]
-echo starting car_control_manual env
+if [ $? -ne 0 ];then
+  echo "error: source ros_catkin_ws/devel/setup.bash failed"
+  exist 1
+fi
+
+echo "source car control manual environment..."
 source /home/flex/slam/src/car_control_manual/scripts/prepare.sh
-echo [ok]
+if [ $? -ne 0 ];then
+  echo "error: source car_control_manual/scripts/prepare.sh failed"
+  exist 1
+fi
 
-echo Running car_control_manual car control script
+echo "Running car control module..."
 python /home/flex/slam/src/car_control_manual/scripts/car.py
-echo [ok]
+if [ $? -ne 0 ];then
+  echo "python car_control_manual/scripts/car.py failed"
+  exist 1
+fi
 
-echo Running cherokey_car car control script
+echo "Running tracking control module..."
 python /home/flex/slam/src/cherokey_car/scripts/car.py
-echo [ok]
+if [ $? -ne 0 ]; then
+  echo "python cherokey_car/scripts/car.py failed"
+  exist 1
+fi
+
+echo "Waiting for GIPO Response.."
+exist 0
